cd7ec6cc83613011870a8f59967af42a23976b16
Emlin <49819627+R4YVEN@users.noreply.github.com>
2023-06-02 11:55:52 +0200

    Update proxy-check.js

--- proxy-check.js
+++ proxy-check.js
@@ -3,7 +3,7 @@ function check_proxy(f) {
         f.apply()
     } catch(e) {
         if(e.stack.includes("Object.apply")) {
-            alert("hook detected on " + f.name)
+            alert("proxy detected on " + f.name)
         }
     }
 }
